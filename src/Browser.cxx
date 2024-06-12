5d2d082e9e392ac877fc19005e91691dda8ba170
mthierman <mthierman@gmail.com>
2024-06-12 14:26:34 -0600

    Move index.html

--- src/Browser.cxx
+++ src/Browser.cxx
@@ -5,7 +5,7 @@
 
 auto Browser::pageAboutToLoad(const juce::String& newUrl) -> bool
 {
-    return newUrl == "http://localhost:5173/" || newUrl == "http://localhost:4173/" ||
+    return newUrl == "http://localhost:5173/" ||
            newUrl == juce::WebBrowserComponent::getResourceProviderRoot();
 }
 
